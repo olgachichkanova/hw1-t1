5754d355e3d9b63bd87a1a372fea11c2f211dda9
Olga Andriychenko <olga@glasnostic.com>
2022-12-08 16:11:28 +0800

    added styles to App.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import ShopItemFunc from "./components/ShopItemFunc"
 import './App.css';
+const data = require('./data/data')
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <div className="background-element">
     </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemFunc item={data.item} />
+    </div>
+  </div>
   );
 }
 
